f8613798a69b0bd37fd99597ed685ce658dce739
Bogdan Djukic <bogdandjukic25@gmail.com>
2023-11-27 22:45:30 +0100

    camera v2

--- Assets/Scripts/camera_controller.cs
+++ Assets/Scripts/camera_controller.cs
@@ -1,22 +1,38 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class camera_controller : MonoBehaviour
 {
-    [SerializeField] private float speed; // Brzina kretanja kamere.
+    // kamera v1 (room-camera)
 
+    [SerializeField] private float speed; // Brzina kretanja kamere.
     // Trenutna X pozicija kamere
     private float currentPosX = -1.13f;
-
     private Vector3 velocity = Vector3.zero; // Vektor brzine kamere.
+
+    // kamera v2 (player-camera)
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float frontDistance;  // Udaljenost za centriranje kamere ispred igraca
+    [SerializeField] private float cameraSpeed; // Brzina pomeranja ka novoj poziciji kamere
+    private float lookAhead;
+    
     private void Update()
     {
-        // Glatko pomeranje kamere prema novoj X poziciji.
+        /*
+        // Glatko pomeranje kamere prema novoj X poziciji - sobi
         transform.position = Vector3.SmoothDamp(
             transform.position, // Trenutna pozicija kamere.
             new Vector3(currentPosX, transform.position.y, transform.position.z), // Nova pozicija
             ref velocity, 
             speed 
         );
+        */
+
+        // Pomeranje kamere naspram igraca
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (frontDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+
     }
 
     // Metoda koja postavlja novu poziciju kamere
