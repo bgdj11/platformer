aa81a5a32cd60f3f9bd6b9670f66bf137ec7eb30
Bogdan Djukic <bogdandjukic25@gmail.com>
2023-12-06 02:20:17 +0100

    iFrames

--- Assets/Scripts/Health/health.cs
+++ Assets/Scripts/Health/health.cs
@@ -1,17 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     private void Awake()
     {
         currentHealth = startingHealth; 
         anim = GetComponent<Animator>(); // Uzima referencu na animator igraca
         dead = false;
+
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -23,7 +32,8 @@ public class health : MonoBehaviour
         {
             // Igrac je povredjen
             anim.SetTrigger("hurt");
-            
+
+            StartCoroutine(Invunerabillity()); 
         }
         else
         {
@@ -34,13 +44,34 @@ public class health : MonoBehaviour
                 GetComponent<player_movement>().enabled = false; // Onemogucava igracu da se pomera kada je mrtav
                 dead = true;
             }
-        }   
+        }  
     }
 
     public void AddHealth(float _val)
     {
         // Povecava helte ukoliko je moguce
         currentHealth = Mathf.Clamp(currentHealth + _val, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerabillity()
+    {   
+
+        Physics2D.IgnoreLayerCollision(8, 9, true);  // Cini igraca neranjivim za vreme iFramesDuration
+
+        // Omogucava da igrac "treperi" numberOfFlashes puta dok je neranjiv
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+
+            yield return new WaitForSeconds(iFramesDuration / numberOfFlashes); // Omogucava period cekanja pre izvrsavanja sledece nardbe
+
+            spriteRend.color = Color.white;
+
+            yield return new WaitForSeconds(iFramesDuration / numberOfFlashes);
+        }
+
+        Physics2D.IgnoreLayerCollision(8, 9, false); // Igrac ponovo postaje ranjiv
+
     }
 
 }
